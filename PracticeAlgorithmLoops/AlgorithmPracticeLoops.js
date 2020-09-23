30b7f4347ed3c68616a4647e178b7764946193c7
Arnold Su <arnoldtingsu@gmail.com>
2020-09-23 00:06:17 -0700

    9/23/2020

---
+++ PracticeAlgorithmLoops/AlgorithmPracticeLoops.js
@@ -0,0 +1,19 @@
+for(var odd = 1; odd < 20; odd+=2){
+    console.log(odd)
+    }
+
+    
+    var sum=0;
+    for( i=0; i<=5; i++){
+        sum+=i
+        console.log('num:', i, 'sum:', sum)
+    }
+    
+// expected:  Num: 1, Sum: 1, Num: 2, Sum: 3, Num: 3, Sum: 6, Num: 4, Sum: 10, Num: 5, Sum: 15
+    
+// num: 0 sum: 0
+// num: 1 sum: 1
+// num: 2 sum: 3
+// num: 3 sum: 6
+// num: 4 sum: 10
+// num: 5 sum: 15
